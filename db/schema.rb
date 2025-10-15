eee2456645a31a603697170a56c6801b23881d42
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 14:12:47 +0530

    The final fix the conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_055453) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_060742) do
+  create_table "book_orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.datetime "created_at", null: false
