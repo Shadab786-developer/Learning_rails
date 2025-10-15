86bbcf9709e9053d5bb4727312ed03a028db5a6d
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 17:10:58 +0530

    I perform the CURD operation in these branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ApplicationRecord
-  self.table_name = "user"
-  self.primary_key = "customer_id"
+  # self.table_name = "user"
+  # self.primary_key = "customer_id"
+
+  def self.perform_curd
+  # PERFORMING AN CURD OPERATION
+
+  # 1. CREATE
+  user = User.create(user_name: "Shadab Qureshi")
+
+  # 2. READE
+  puts "All user :"
+  puts User.all.inspect
+  puts "Find by id :"
+  puts User.find(2).inspect
+
+  # 3. UPDATE
+  user.update(user_name: "Harish Ansari")
+  puts "After Update :"
+  puts User.find(2).inspect
+
+  # 4. DELETING
+  user.destroy
+  puts "After Delete :"
+  puts User.all.inspect
+end
 end
