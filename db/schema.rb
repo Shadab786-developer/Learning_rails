06c6d33359ee54e27b07ca494ba2431a723d7038
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 18:38:54 +0530

    I perform the Association operation in these branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_060742) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_121150) do
   create_table "book_orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
