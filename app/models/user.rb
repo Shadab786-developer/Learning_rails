e37b3a87d4eae26432d863d040e6e346d5f509b8
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 16:38:31 +0530

    Fix the rubocop lint

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   self.table_name = "user"
   self.primary_key = "customer_id"
 
-  #THERE IS THE VALIDATION WHICH IS DECIDING WHEN THE DATA WAS SAVE INTO THE DATA BASE THERE ARE MANY TYPE OF VALIDATION LIKE uniquness , confirm, custom etc I USE THE PRESENCE VALIDATION HERE. 
+  # THERE IS THE VALIDATION WHICH IS DECIDING WHEN THE DATA WAS SAVE INTO THE DATA BASE THERE ARE MANY TYPE OF VALIDATION LIKE uniquness , confirm, custom etc I USE THE PRESENCE VALIDATION HERE.
   validates :user_name, presence: true
 
-  #THESE IS THE CALLBACK METHOD WHICH IS COMES INTO THE ACTIVE RECORD THERE ARE MANY TYPE OF CALLBACK MAINLY ARE after, before , around etc
+  # THESE IS THE CALLBACK METHOD WHICH IS COMES INTO THE ACTIVE RECORD THERE ARE MANY TYPE OF CALLBACK MAINLY ARE after, before , around etc
 
   after_create :log_new_user
 
@@ -13,26 +13,25 @@ class User < ApplicationRecord
     def log_new_user
       puts "A new user was registered"
     end
-  
-  def self.perform_curd
-    
-  # PERFORMING AN CURD OPERATION 
 
-  #1. CREATE
+  def self.perform_curd
+  # PERFORMING AN CURD OPERATION
+
+  # 1. CREATE
   user = User.create(user_name: "Shadab Qureshi")
 
-  #2. READE
+  # 2. READE
   puts "All user :"
   puts User.all.inspect
   puts "Find by id :"
   puts User.find(1).inspect
 
-  #3. UPDATE
+  # 3. UPDATE
   user.update(user_name: "Harish Ansari")
   puts "After Update :"
   puts User.find(1).inspect
- 
-  #4. DELETING
+
+  # 4. DELETING
   user.destroy
   puts "After Delete :"
   puts User.all.inspect
