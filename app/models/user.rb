11e55a06f97830379a1fa8eca1b6e0686bfd144b
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 18:28:59 +0530

    I perform the Association operation in these branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,31 +9,31 @@ class User < ApplicationRecord
 
   after_create :log_new_user
 
+  
+  def self.perform_curd
+    # PERFORMING AN CURD OPERATION
+    
+    # 1. CREATE
+    user = User.create(user_name: "Shadab Qureshi")
+    
+    # 2. READE
+    puts "All user :"
+    puts User.all.inspect
+    puts "Find by id :"
+    puts User.find(2).inspect
+    
+    # 3. UPDATE
+    user.update(user_name: "Harish Ansari")
+    puts "After Update :"
+    puts User.find(2).inspect
+    
+    # 4. DELETING
+    user.destroy
+    puts "After Delete :"
+    puts User.all.inspect
+  end
   private
     def log_new_user
       puts "A new user was registered"
     end
-
-  def self.perform_curd
-  # PERFORMING AN CURD OPERATION
-
-  # 1. CREATE
-  user = User.create(user_name: "Shadab Qureshi")
-
-  # 2. READE
-  puts "All user :"
-  puts User.all.inspect
-  puts "Find by id :"
-  puts User.find(2).inspect
-
-  # 3. UPDATE
-  user.update(user_name: "Harish Ansari")
-  puts "After Update :"
-  puts User.find(2).inspect
-
-  # 4. DELETING
-  user.destroy
-  puts "After Delete :"
-  puts User.all.inspect
-end
 end
