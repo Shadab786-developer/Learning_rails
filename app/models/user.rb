2f4ea52bef99d766a1c36cc077f16044f2798ff0
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-15 16:08:49 +0530

    I perform the callback operation function in Active record

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,20 @@ class User < ApplicationRecord
   self.table_name = "user"
   self.primary_key = "customer_id"
 
-  #THERE IS THE VALIDATION WHICH IS DECIDING WHEN THE DATA WAS SAVE INTO THE DATA BASE THERE ARE MANY TYPE OF VALIDATION LIKE uniquness , confirm, custom etc I USE THE PRESENCE VALIDATION HERE.
-  validates user_name: presence: true
+  #THERE IS THE VALIDATION WHICH IS DECIDING WHEN THE DATA WAS SAVE INTO THE DATA BASE THERE ARE MANY TYPE OF VALIDATION LIKE uniquness , confirm, custom etc I USE THE PRESENCE VALIDATION HERE. 
+  validates :user_name, presence: true
 
+  #THESE IS THE CALLBACK METHOD WHICH IS COMES INTO THE ACTIVE RECORD THERE ARE MANY TYPE OF CALLBACK MAINLY ARE after, before , around etc
 
+  after_create :log_new_user
+
+  private
+    def log_new_user
+      puts "A new user was registered"
+    end
+  
   def self.perform_curd
-
+    
   # PERFORMING AN CURD OPERATION 
 
   #1. CREATE
@@ -28,5 +36,5 @@ class User < ApplicationRecord
   user.destroy
   puts "After Delete :"
   puts User.all.inspect
-  end
+end
 end
