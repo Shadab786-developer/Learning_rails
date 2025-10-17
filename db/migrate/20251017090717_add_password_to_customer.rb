7d31ab7d90dca638ddbf426c3c30ab1c48aba976
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-10-17 18:37:06 +0530

    some changes done

--- db/migrate/20251017090717_add_password_to_customer.rb
+++ db/migrate/20251017090717_add_password_to_customer.rb
@@ -2,6 +2,5 @@ class AddPasswordToCustomer < ActiveRecord::Migration[8.0]
   def change
     add_column :customers, :password, :string
     add_column :customers, :password_confirmation, :string
-
   end
 end
